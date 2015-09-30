64c2cba6fa6fb5852e76e620e2631e2347dee20d
Yun-Jhong Wu <yjwu@umich.edu>
2015-09-29 23:06:09 -0400

    Rewrite running time

--- evaluator.py
+++ evaluator.py
@@ -4,48 +4,79 @@
 # Author: Yun-Jhong Wu
 # E-mail: yjwu@umich.edu
 
-from os import chdir, listdir, remove
+from os import chdir, listdir, remove, walk
 from shutil import copyfile
 from timeit import timeit
 from numpy import median
 from subprocess import Popen, PIPE
 from sys import path
+
 import random
+
+#from task_checker import task_checker
 
 if __name__ == '__main__':
     path.append("../../modules")
     filenames = ['assignment_one_kmeans.py',
                  'assignment_one_nearest_neighbor.py',
                  'assignment_one_optimization.py']
-    dataname = "seeds_dataset.txt"
+    suppl = ['seeds_dataset.txt', 'seeds_dataset_shuffled.txt', 'losses.py']
+    banned = ['numpy', 'scipy', 'sklearn', 'cvxopt']
+    
     performance = []
-    eed = random.randint(0, 65535)
-    for folder in listdir("hw"):
-        random.seed(seed)
+    seed = random.randint(0, 65535)
+    for folder in listdir("hw1"):
         _, _, uniquename = folder.split('_')
-        pathname = "hw/assignment_one_{0}/".format(uniquename)
-        copyfile("datasets/{0}".format(dataname), pathname + dataname)
-        t = [float('inf')] * 3
-        pep8_pass = True
-        chdir(pathname)
+        pathname = "hw1/assignment_one_{0}/".format(uniquename)
+        comment = []
+        # Find scripts and move them to assignment_one_{uniquename}
+        for p, _, files in walk(pathname):
+            for file in files:
+                if file in filenames:
+                    try:
+                        copyfile('{0}/{1}'.format(p, file), pathname + file)
+                    except:
+                        pass
+                    
+                with open('{0}/{1}'.format(pathname, file)) as script:
+                    for line in script:
+                        for package in banned:
+                            if package in line:
+                                comment.append('banned')
+                            
+                    
+        for s in suppl: copyfile("suppl/{0}".format(s), pathname + s)
 
-        for i, task in enumerate(filenames):
-            try:
-                script = "exec(open('{0}').read())".format(task)
-                t[i] = median([timeit(script, number=1) for _ in range(10)])
-                print("{0}, {1}: Evaluated".format(uniquename, task))
-            except:
-                print("{0}, {1}: Failed".format(uniquename, task))
-
-            pep8_result = Popen(["pep8", task], stdout=PIPE).communicate()[0]
-            pep8_pass &= len(pep8_result) == 0
-
-        remove(dataname)
+        # Variables for storing results        
+        running_time = []        
+        results = [] #task_checker(pathname)
+        
+        chdir(pathname)        
+        random.seed(seed)
+        if all(results):
+            for i, task in enumerate(filenames):
+                script = "call(['python', '{0}'])".format(task)
+                try: 
+                    t = [timeit(script, number=1, 
+                                setup='from subprocess import call') 
+                                for _ in range(5)]
+                    running_time.append(median(t))
+                    print("{0}, {1}: Evaluated".format(uniquename, task))
+                except:
+                    running_time.append(float('inf'))
+                    print("{0}, {1}: Failed".format(uniquename, task))
+            
+        pep8_failed = [len(Popen(["pep8", task], stdout=PIPE).communicate()[0])
+                       for task in filenames]
+        for s in suppl: remove(s)
         chdir("../..")
-        performance.append((sum(t), t[0], t[1], t[2], pep8_pass, uniquename))
+        
+        performance.append((uniquename, 
+                            ' '.join(map(str, running_time)), 
+                            ' '.join(map(str, pep8_failed)), 
+                            ' '.join(map(str, results)),
+                            ' '.join(comment)))
 
-    performance.sort()
     with open("evaluation.txt", "w") as output:
         for student in performance:
-            output.write("{0} {1}\n".format(student[-1],
-                                            ' '.join(map(str, student[:-1]))))
+            output.write("{0} {1}\n".format(student))
